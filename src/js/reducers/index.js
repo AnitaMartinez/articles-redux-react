5a4dd02a3805eff061bed36b7f54ad22c7f24fc8
Ana Martínez <amartinezaguilar6@gmail.com>
2019-01-29 18:19:16 +0100

    Create reducer

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,9 +1,15 @@
+import { ADD_ARTICLE } from '/src/js/constants/action-types';
 
 const initialState = {
     articles : []
 };
 
 function rootReducer(state = initialState, action) {
+    if(action.type === ADD_ARTICLE) {
+        return Object.assign({}, state, {
+            articles : state.articles.concat(action.payload)
+        });
+    }
     return state;
 }
 
